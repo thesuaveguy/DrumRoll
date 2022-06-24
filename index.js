9f2a23b707f70b58b987564f7a26bea63121195e
thesuaveguy <lit2021026@iiit.ac.in>
2022-06-24 15:41:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+//event press
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+    
+  });
+}
+
+
+//keyboard press
+// document.addEventListener('keydown',function(e){
+//     makeSound(e.key);
+//   });
+document.addEventListener('keydown',(e)=>{
+    makeSound(e.key);
+    buttonAnimation(e.key);
+  });
+  
+// function evl(e){
+//   // console.log(e.key);
+//   makeSound(e.key);
+// }
+//   document.addEventListener('keydown', function(e){
+//     evl(e);
+//     console.log(e.key);
+//   });
+ 
+
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    default:
+      console.log(e.key);
+
+}
+}
+
+function buttonAnimation(currentKey){
+  var activeButton=document.querySelector(`.${currentKey}`);
+  activeButton.classList.add('pressed');
+  setInterval(function(){
+    activeButton.classList.remove('pressed');
+  },100)
+
+}
+
+ 
